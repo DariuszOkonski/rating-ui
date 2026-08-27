325ad158661008385ce06ea192a012070078fde5
Dariusz Okonski <darek200180@gmail.com>
2026-08-27 07:08:23 +0200

    remove handlers

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,23 +1,18 @@
 const Rating = () => {
-  const stars = Array.from({length: 5}, (_, i) => i + 1)
-
-  const hovered = (index) => console.log('hovered: ', index)
-
-  const leaved = (index) => console.log("leaved: ", index)
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star, index) => (
-          <span 
-            onMouseEnter={() => hovered(index)} key={star} className="star"
-            onMouseLeave={() => leaved(index)}
-            >{'\u2605'}</span>
+        {stars.map((star) => (
+          <span key={star} className="star">
+            {"\u2605"}
+          </span>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Rating;

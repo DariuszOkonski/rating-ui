44a4e4a8893423cd91e51c5c3d6ca4b37ede2247
Dariusz Okonski <darek200180@gmail.com>
2026-08-27 06:52:49 +0200

    add events on span

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,19 @@
 const Rating = () => {
   const stars = Array.from({length: 5}, (_, i) => i + 1)
 
+  const hovered = (index) => console.log('hovered: ', index)
+
+  const leaved = (index) => console.log("leaved: ", index)
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star) => (
-          <span key={star} className="star">{'\u2605'}</span>
+        {stars.map((star, index) => (
+          <span 
+            onMouseEnter={() => hovered(index)} key={star} className="star"
+            onMouseLeave={() => leaved(index)}
+            >{'\u2605'}</span>
         ))}
       </div>
     </div>

4fbff26d5c1da61ef19194786cc994e17103800e
Dariusz Okonski <darek200180@gmail.com>
2026-08-28 06:26:50 +0200

    add Counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Counter from "./components/Counter";
 import Rating from "./components/Rating";
 
 function App() {
   return (
     <div>
       <Rating />
+      <Counter />
     </div>
   );
 }

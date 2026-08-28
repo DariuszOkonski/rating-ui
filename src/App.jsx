2e0a7426b207b8c29940449a399ba1475ac0515f
Dariusz Okonski <darek200180@gmail.com>
2026-08-28 06:48:01 +0200

    add feedbackMessages as props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import Rating from "./components/Rating";
 function App() {
   return (
     <div>
-      <Rating heading="How do you feel about React?" color="blue" />
+      <Rating
+        heading="How do you feel about React?"
+        color="blue"
+        feedbackMessages={[
+          "Hate It",
+          "Dislike it",
+          "Meh",
+          "Like it",
+          "Love it",
+        ]}
+      />
     </div>
   );
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
-const Rating = ({ heading = "Rate Your Experience", color = "gold" }) => {
+const Rating = ({
+  heading = "Rate Your Experience",
+  color = "gold",
+  feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"],
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 

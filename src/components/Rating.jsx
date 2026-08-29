048382e40734c878f622dbb748386f782dbfc8a1
Dariusz Okonski <darek200180@gmail.com>
2026-08-29 12:14:12 +0200

    add modal component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Star from "./Star";
+import Modal from "./Modal";
 
 const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
@@ -50,19 +51,7 @@ const Rating = ({
         Submit
       </button>
 
-      {submitted && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <h2>Thank You</h2>
-            <p>
-              You rated us: {rating} star{rating > 1 ? "s" : ""}
-            </p>
-            <button className="close-btn" onClick={closeModal}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      <Modal isOpen={submitted} onClose={closeModal} rating={rating} />
     </div>
   );
 };
